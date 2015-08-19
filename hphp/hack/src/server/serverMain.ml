5967951d12ceb7d604fa9e27670d0ceb58275941
Cagdas Bozman <cagdas.bozman@ocamlpro.com>
2015-08-19 16:37:27 +0200

    timeout: proper timout handling on Windows

--- hphp/hack/src/server/serverMain.ml
+++ hphp/hack/src/server/serverMain.ml
@@ -320,10 +320,9 @@ let run_load_script genv cmd =
         (Filename.quote Build_id.build_id_ohai) in
     Hh_logger.log "Running load script: %s\n%!" cmd;
     let state_fn, to_recheck =
-      let do_fn () =
-        let ic = Unix.open_process_in cmd in
+      let reader timeout ic _oc =
         let state_fn =
-          try input_line ic
+          try Timeout.input_line ~timeout ic
           with End_of_file -> raise State_not_found
         in
         if state_fn = "DISABLED" then raise Load_state_disabled;
@@ -331,18 +330,19 @@ let run_load_script genv cmd =
         begin
           try
             while true do
-              to_recheck := input_line ic :: !to_recheck
+              to_recheck := Timeout.input_line ~timeout ic :: !to_recheck
             done
           with End_of_file -> ()
         end;
-        assert (Unix.close_process_in ic = Unix.WEXITED 0);
+        let rc = Timeout.close_process_in ic in
+        assert (rc = Unix.WEXITED 0);
         state_fn, !to_recheck
       in
-      with_timeout
-        (ServerConfig.load_script_timeout genv.config)
+      Timeout.read_process
+        ~timeout:(ServerConfig.load_script_timeout genv.config)
         ~on_timeout:(fun _ -> failwith "Load script timed out")
-        ~do_:do_fn
-    in
+        ~reader
+        cmd [| cmd |] in
     Hh_logger.log
       "Load state found at %s. %d files to recheck\n%!"
       state_fn (List.length to_recheck);
